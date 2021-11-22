98bd8c39aee0b806edc87ddfa25a2a7c33c2f835
katigarnikhita <nikhitakatigarxworksbtm@gmail.com>
2021-11-22 16:45:07 +0530

    first commit

---
+++ Developer.java
@@ -0,0 +1,16 @@
+class Developer
+{
+String name;
+String Designation;
+int salary;
+int TotalExp;
+
+Developer(String x,String y,int Developer z,int Developer w)
+{
+name=x;
+Designation=y;
+salary=z;
+TotalExp=w;
+}
+}
+
